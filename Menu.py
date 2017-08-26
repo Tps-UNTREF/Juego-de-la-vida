b93c0bf67624081d34f9bb292bcc0b13a2ea5d9e
Matias Juguera <mati.juguera@gmail.com>
2017-08-26 17:25:42 -0300

    Se agregaron los metodos leer_teclado y leer_entero en la clase menu

---
+++ Menu.py
@@ -0,0 +1,24 @@
+class Menu(object):
+
+    def Menu(self):
+        tamaño_de_tablero = self.leer_entero(self.leer_teclado('Ingrese el tamaño del tablero: '),'Ingrese el tamaño del tablero: ')
+
+    def leer_teclado(self,texto):
+        try:
+            ingresado = eval(input(texto))
+            if (ingresado == ''):
+                return None
+            else:
+                return ingresado
+        except (EOFError, KeyboardInterrupt):
+            return None
+
+    def leer_entero(self,entrada,texto):
+        try:
+            if type(entrada) == int:
+                return entrada
+            else:
+                raise Exception
+        except Exception:
+            print('Por favor ingrese un numero entero')
+            return self.leer_entero(self.leer_teclado(texto))
