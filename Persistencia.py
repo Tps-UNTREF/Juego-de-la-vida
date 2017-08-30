e57343cd4a5018be4b943748c2114754ff90f4f6
matt1bell <matt>
2017-08-31 01:04:27 +0200

    se modif. class Persistencia y se creo class TestPersistencia (vacia)

--- Persistencia.py
+++ Persistencia.py
@@ -1,8 +1,28 @@
 import shelve
 class Persistencia(object):
 
-    def guardar(self,ruta):
-        pass
+    def guardar(self, ruta , tablero_celular, clave):
+        modulo_guardado = shelve.open(ruta)
 
-    def cargar(self,ruta):
-        pass
+        if modulo_guardado[clave] == None:
+
+           modulo_guardado[clave] = tablero_celular
+           print("-------" + "tablero guardado con exito" + "--------")
+           modulo_guardado.close()
+        else:
+            modulo_guardado[clave] = tablero_celular
+            print("-------" + "tablero sobreescrito con exito" + "--------")
+            modulo_guardado.close()
+
+
+    def cargar(self, ruta, clave):
+        tablero_celular = None
+        modulo_a_cargar = shelve.open(ruta)
+
+        if modulo_a_cargar[clave] != None:
+
+            tablero_celular = modulo_a_cargar[clave]
+            print("------" + "tablero cargado con exito" + "-------" )
+            return tablero_celular
+        else:
+            print("-------" + "no existe tablero guardado en la clave" + " " + str(clave) + "--------")
