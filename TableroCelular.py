52954364a7bbec37076759b7a977c5b5523e1b83
Matias Juguera <mati.juguera@gmail.com>
2017-08-30 01:09:15 -0300

    -se modifico el metodo de rellenar celdas al azar

--- TableroCelular.py
+++ TableroCelular.py
@@ -6,17 +6,27 @@ class TableroCelular(object):
     def __init__(self,numero_columnas_filas):
         self.matriz = []
         self.numero_columnas_filas = numero_columnas_filas
-        self.creacion_de_la_matriz(numero_columnas_filas)
+        self.crear_matriz_vacia(numero_columnas_filas)
 
-    def creacion_de_la_matriz(self,numero_columnas_filas):
+    def crear_matriz_vacia(self,numero_columnas_filas):
         self.matriz = []
         for i in range(numero_columnas_filas):
             self.matriz.append(['-'] * numero_columnas_filas)
 
-    def rellenar_matriz_al_azar(self):
-        for i in range(self.numero_columnas_filas):
-            for j in range(self.numero_columnas_filas):
-                self.matriz[i][j] = random.choice(['*','-'])
+    def rellenar_matriz_al_azar(self,numero_de_celdas_vivas):
+        combinaciones = []
+
+        if(numero_de_celdas_vivas <= len(self.matriz)*len(self.matriz)):
+            for fila in range(len(self.matriz)):
+                for columna in range(len(self.matriz)):
+                    combinaciones.append((fila,columna))
+            random.shuffle(combinaciones)
+            for i in range(numero_de_celdas_vivas):
+                combinacion_random = combinaciones.pop()
+                self.matriz[combinacion_random[0]][combinacion_random[1]] = '*'
+        else:
+            raise IndexError
+
 
     def rellenar_matriz_manualmente(self,fila,columna,valor_de_matriz):
         if(fila > self.numero_columnas_filas or columna > self.numero_columnas_filas):
@@ -25,6 +35,8 @@ class TableroCelular(object):
             raise KeyError #cambiar error
         else:
             self.matriz[fila][columna] = valor_de_matriz
+
+
 
 
 
--- TestTableroCelular.py
+++ TestTableroCelular.py
@@ -9,9 +9,16 @@ class TestTableroCelular(unittest.TestCase):
         print('Matriz Vacia')
         print(self.tablero.matriz)
 
-    def test_rellenar_matriz_al_azar(self):
+    def test_rellenar_matriz_al_azar_completo(self):
+        '''La idea es llenar la matriz para ver que no se pisen las posiciones'''
         print('Matriz Random')
-        self.tablero.rellenar_matriz_al_azar()
+        self.tablero.rellenar_matriz_al_azar(16)
+        print(self.tablero.matriz)
+
+    def test_rellenar_matriz_al_azar(self):
+        self.tablero.crear_matriz_vacia(4)
+        print('Matriz Random 4 celulas vivas')
+        self.tablero.rellenar_matriz_al_azar(4)
         print(self.tablero.matriz)
 
 
