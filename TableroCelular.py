5aa930b5863fa5510d663f036e1c7f83424aac02
nicohansen <nicolas.han.sen@hotmail.com>
2017-09-09 17:06:26 -0300

    Se creo el metodo para mutar celdas.

--- TableroCelular.py
+++ TableroCelular.py
@@ -2,16 +2,15 @@ import random
 
 class TableroCelular(object):
 
-
     def __init__(self,numero_columnas_filas):
-        self.matriz = []
+        self.matriz = self.matriz_nueva(numero_columnas_filas)
         self.numero_columnas_filas = numero_columnas_filas
-        self.crear_matriz_vacia(numero_columnas_filas)
 
-    def crear_matriz_vacia(self,numero_columnas_filas):
-        self.matriz = []
+    def matriz_nueva(self,numero_columnas_filas):
+        matriz_nueva = []
         for i in range(numero_columnas_filas):
-            self.matriz.append(['-'] * numero_columnas_filas)
+            matriz_nueva.append(['-'] * numero_columnas_filas)
+        return matriz_nueva
 
     def rellenar_matriz_al_azar(self,numero_de_celdas_vivas):
         combinaciones = []
@@ -39,4 +38,31 @@ class TableroCelular(object):
     def imprimir_tablero(self):
         for f in self.matriz:
             print(str(f))
+
+    def cantidad_de_vecinos(self, fila, columna):
+        distancia_de_celdas = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        celdas_vivas_alrededor = 0
+        for x, y in distancia_de_celdas:
+            if (x >= 0 and x < len(self.matriz) and (y >= 0 and y < len(self.matriz))): # Verifica que este dentro del tablero
+                if(self.matriz[fila + x][columna + y] == '*'):
+                    celdas_vivas_alrededor += 1
+        return celdas_vivas_alrededor
+
+    def mutar_celulas(self):
+        matriz_actualizada = self.matriz_nueva(len(self.matriz))
+        for x, y in self.matriz:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
